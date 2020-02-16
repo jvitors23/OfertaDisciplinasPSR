787abab95b343dea639efdab320ad4f57323f558
helisson <helissonmag@hotmail.com>
2020-02-15 23:48:22 -0300

    com classe Horario

--- src/main/CSPturmaToHorario/Horario.java
+++ src/main/CSPturmaToHorario/Horario.java
@@ -12,12 +12,10 @@ import java.util.ArrayList;
  * @author helis
  */
 public class Horario {
-    String aula1, aula2, aula3;
+    public String aulas[];
 
-    public Horario(String aula1, String aula2, String aula3) {
-        this.aula1 = aula1;
-        this.aula2 = aula2;
-        this.aula3 = aula3;
+    public Horario(String[] aulas) {
+        this.aulas = aulas;
     }
     
     
@@ -133,46 +131,46 @@ public class Horario {
                 domain.add(new Horario("6T12","6T34",""));
                 domain.add(new Horario("6T34","6T56",""));
                 //noite dois dias diferentes
-                domain.add(new Horario("2N12","4N12",""));
-                domain.add(new Horario("2N34","4N34",""));
-                domain.add(new Horario("3N12","5N12",""));
-                domain.add(new Horario("3N34","5N34",""));
-                domain.add(new Horario("4N12","6N12",""));
-                domain.add(new Horario("4N34","6N34",""));
+                domain.add(new Horario(new String[]{"2N12","4N12",""}));
+                domain.add(new Horario(new String[]{"2N34","4N34",""}));
+                domain.add(new Horario(new String[]{"3N12","5N12",""}));
+                domain.add(new Horario(new String[]{"3N34","5N34",""}));
+                domain.add(new Horario(new String[]{"4N12","6N12",""}));
+                domain.add(new Horario(new String[]{"4N34","6N34",""}));
                 
-                domain.add(new Horario("2N12","5N12",""));
-                domain.add(new Horario("2N34","5N34",""));
-                domain.add(new Horario("3N12","6N12",""));
-                domain.add(new Horario("3N34","6N34",""));
-                domain.add(new Horario("2N12","6N12",""));
-                domain.add(new Horario("2N34","6N34",""));
+                domain.add(new Horario(new String[]{"2N12","5N12",""}));
+                domain.add(new Horario(new String[]{"2N34","5N34",""}));
+                domain.add(new Horario(new String[]{"3N12","6N12",""}));
+                domain.add(new Horario(new String[]{"3N34","6N34",""}));
+                domain.add(new Horario(new String[]{"2N12","6N12",""}));
+                domain.add(new Horario(new String[]{"2N34","6N34",""}));
                 //noite mesmo dia
-                domain.add(new Horario("2N12","2N34",""));
-                domain.add(new Horario("3N12","3N34",""));
-                domain.add(new Horario("4N12","4N34",""));
-                domain.add(new Horario("5N12","5N34",""));
-                domain.add(new Horario("6N12","6N34",""));
+                domain.add(new Horario(new String[]{"2N12","2N34",""}));
+                domain.add(new Horario(new String[]{"3N12","3N34",""}));
+                domain.add(new Horario(new String[]{"4N12","4N34",""}));
+                domain.add(new Horario(new String[]{"5N12","5N34",""}));
+                domain.add(new Horario(new String[]{"6N12","6N34",""}));
              
             }else{
                 if(numCreditos == 6){
-                    domain.add(new Horario("2M12","4M12","6M12"));
-                    domain.add(new Horario("2M34","4M34","6M34"));
-                    domain.add(new Horario("2M56","4M56","6M56"));
-                    domain.add(new Horario("3M12","5M12","6M12"));
-                    domain.add(new Horario("3M34","5M34","6M34"));
-                    domain.add(new Horario("3M56","5M56","6M56"));
+                    domain.add(new Horario(new String[]{"2M12","4M12","6M12"}));
+                    domain.add(new Horario(new String[]{"2M34","4M34","6M34"}));
+                    domain.add(new Horario(new String[]{"2M56","4M56","6M56"}));
+                    domain.add(new Horario(new String[]{"3M12","5M12","6M12"}));
+                    domain.add(new Horario(new String[]{"3M34","5M34","6M34"}));
+                    domain.add(new Horario(new String[]{"3M56","5M56","6M56"}));
                     
-                    domain.add(new Horario("2T12","4T12","6T12"));
-                    domain.add(new Horario("2T34","4T34","6T34"));
-                    domain.add(new Horario("2T56","4T56","6T56"));
-                    domain.add(new Horario("3T12","5T12","6T12"));
-                    domain.add(new Horario("3T34","5T34","6T34"));
-                    domain.add(new Horario("3T56","5T56","6T56"));
+                    domain.add(new Horario(new String[]{"2T12","4T12","6T12"}));
+                    domain.add(new Horario(new String[]{"2T34","4T34","6T34"}));
+                    domain.add(new Horario(new String[]{"2T56","4T56","6T56"}));
+                    domain.add(new Horario(new String[]{"3T12","5T12","6T12"}));
+                    domain.add(new Horario(new String[]{"3T34","5T34","6T34"}));
+                    domain.add(new Horario(new String[]{"3T56","5T56","6T56"}));
                     
-                    domain.add(new Horario("2N12","4N12","6N12"));
-                    domain.add(new Horario("2N34","4N34","6N34"));
-                    domain.add(new Horario("3N12","5N12","6N12"));
-                    domain.add(new Horario("3N34","5N34","6N34"));
+                    domain.add(new Horario(new String[]{"2N12","4N12","6N12"}));
+                    domain.add(new Horario(new String[]{"2N34","4N34","6N34"}));
+                    domain.add(new Horario(new String[]{"3N12","5N12","6N12"}));
+                    domain.add(new Horario(new String[]{"3N34","5N34","6N34"}));
                     
                   
                 }
@@ -180,11 +178,11 @@ public class Horario {
         }
         
         if(numCreditos == 4){
-            domain.remove(new Horario(calculo[0],calculo[1], ""));
-            domain.remove(new Horario(fisica[0],fisica[1], ""));
+            domain.remove(new Horario(new String[]{calculo[0],calculo[1], ""}));
+            domain.remove(new Horario(new String[]{fisica[0],fisica[1], ""}));
         }
         
         return domain;
     }
-    
+
 }
